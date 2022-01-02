a584c4bdfdc3029693b8af520fe579421194441b
Robert Warren <warrenrobert127@gmail.com>
2022-01-02 10:44:23 -0600

    add .env file and database

---
+++ models/ProductTag.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true
+    },
+      tag_id:{
+        type:DataTypes.STRING,
+        allowNull:true,
+        references: {
+          model: "tag",
+          key: "id"
+        }
+      
+      },
+      product_id:{
+        type:DataTypes.STRING,
+        allowNull:true,
+        references: {
+          model: "product",
+          key: "id"
+        }
+      },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
